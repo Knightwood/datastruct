0e14f09c3233f870706e277c4d35efb38b22f72f
Knightwood <kiylx@outlook.com>
2019-12-11 23:20:19 +0800

    修改逻辑

--- SampleDownload/test/DownloadManagerTest.java
+++ SampleDownload/test/DownloadManagerTest.java
@@ -1,30 +1,32 @@
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+import static org.junit.Assert.*;
 
-    public static void main(String[] args) {
+public class DownloadManagerTest {
+
+    @Test
+    public void startDownload() {
+    }
+
+    @Test
+    void download(String[] args) {
         System.out.println("Hello World!");
         //list被new出来以后，不添加任何数据的时候就是null，所以用list.get(0)会报错，要用list.isEmpty
         List list = new ArrayList<String>();
         list.add("adwd");
         System.out.println(list.isEmpty());
 
-        DownloadManager manager=DownloadManager.getInstance();
-        DownloadInfo info1=new DownloadInfo("https://raw.githubusercontent.com/guolindev/eclipse/master/eclipse-inst-win64.exe",null,null,8);
+        DownloadManager manager = DownloadManager.getInstance();
+        DownloadInfo info1 = new DownloadInfo("https://raw.githubusercontent.com/guolindev/eclipse/master/eclipse-inst-win64.exe", null, null, 8);
 
         try {
             manager.startDownload(info1);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
     }
 }
